7d765ba274ab7efa63d864f10d180d0f54937d4e
Cyberium <cybermomo@techemail.com>
2016-03-05 05:09:53 +0100

    Initial commit

---
+++ avtest/avtest.cpp
@@ -0,0 +1,14 @@
+// avtest.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+
+
+int main()
+{
+    printf("Hello world\r\n");
+    system("pause");
+    return 0;
+}
+
